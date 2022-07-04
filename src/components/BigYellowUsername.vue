32c569ed29bc2eca49031fd2f9fc6a8ac73f420d
AnaAlb1 <92902419+AnaAlb1@users.noreply.github.com>
2022-07-04 18:01:28 -0500

    Add files via upload

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="username">{{ username }}</p>
+</template>
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.username {
+  font-size: 50pt;
+  color: goldenrod;
+}
+</style>
